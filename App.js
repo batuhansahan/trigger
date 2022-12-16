25f1198ef6dde364cd1c74f9bf2b81e844d2e374
Batuhan Şahan <turkwars@gmail.com>
2022-12-16 20:22:36 +0300

    light example

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import React, { useEffect } from 'react';
-import {
-  SafeAreaView,
-  Text,
-  TouchableOpacity,
-} from 'react-native';
-import notifee, { TimestampTrigger, TriggerType, AndroidCategory, AndroidImportance, EventType } from '@notifee/react-native';
+import React, {useEffect} from 'react';
+import {SafeAreaView, Text, TouchableOpacity} from 'react-native';
+import notifee, {
+  TimestampTrigger,
+  TriggerType,
+  AndroidCategory,
+  AndroidImportance,
+  EventType,
+  AndroidColor,
+} from '@notifee/react-native';
 
-
-notifee.onBackgroundEvent(async ({ type, detail }) => {
-  const { notification, pressAction } = detail;
+notifee.onBackgroundEvent(async ({type, detail}) => {
+  const {notification, pressAction} = detail;
   console.log('BACKGROUND EVENT', type, detail);
 });
-
 
 const App = () => {
   useEffect(() => {
@@ -19,49 +20,61 @@ const App = () => {
       const scheduled = await notifee.getTriggerNotifications();
       console.log('### SCHEDULED NOTIFICATIONS ##', scheduled);
     }
-    boot()
-  }, [])
+    boot();
+  }, []);
 
-  const onCreateNotification = async (min) => {
-    const date = new Date(Date.now());
-    date.setHours(date.getHours());
-    date.setMinutes(date.getMinutes() + min); // 2 minutes from now
-    date.setSeconds(0)
+  const onCreateNotification = async (sec, lights) => {
+    const date = new Date().getTime() + sec * 1000;
     // Create a time-based trigger
     const trigger = {
       type: TriggerType.TIMESTAMP,
-      timestamp: date.getTime(), // fire in 1 minute
-      alarmManager: true
+      timestamp: date, // fire in 1 minute
+      alarmManager: true,
     };
     await notifee.createChannel({
       id: '123',
       name: '123',
       sound: 'default',
       importance: AndroidImportance.HIGH,
+      lightColor: AndroidColor.PURPLE,
+      lights: true,
     });
+
+    const androidConfigWithLights = {
+      channelId: '123',
+      lights: [AndroidColor.PURPLE, 1000, 1000],
+    };
+
+    const androidConfigWithoutLights = {
+      channelId: '123',
+    };
 
     // Create a trigger notification
     await notifee.createTriggerNotification(
       {
         title: 'Meeting with Jane',
         body: 'Today at 11:20am',
-        android: {
-          channelId: '123',
-        },
+        android: lights ? androidConfigWithLights : androidConfigWithoutLights,
       },
       trigger,
     );
-    console.log(date)
-  }
+    console.log(date);
+  };
 
   return (
     <SafeAreaView>
-      <TouchableOpacity style={{ marginTop: 50 }} onPress={() => onCreateNotification(1)}><Text>Create Schuduled Notification(fires in 1min)</Text></TouchableOpacity>
-      <TouchableOpacity style={{ marginTop: 50 }} onPress={() => onCreateNotification(2)}><Text>Create Schuduled Notification(fires in 2min)</Text></TouchableOpacity>
-      <TouchableOpacity style={{ marginTop: 50 }} onPress={() => onCreateNotification(3)}><Text>Create Schuduled Notification(fires in 3min)</Text></TouchableOpacity>
+      <TouchableOpacity
+        style={{marginTop: 50}}
+        onPress={() => onCreateNotification(5, false)}>
+        <Text>Create Notification Without Lights(fires in 5 sec)</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{marginTop: 50}}
+        onPress={() => onCreateNotification(10, true)}>
+        <Text>Create Notification With Lights(fires in 5 sec)</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
-
 
 export default App;
